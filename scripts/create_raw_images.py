2ed8760f04646d8c1cee0067719d4080878e08ba
Ignas Budvytis <ib255@cam.ac.uk>
2019-05-01 12:03:59 +0100

    adapting raw images

--- scripts/create_raw_images.py
+++ scripts/create_raw_images.py
@@ -1,23 +1,24 @@
-from scipy import misc
+import cv2
 import sys
 import os
 import numpy as np
 import glob
 
-def performConversion(input_directory_name,output_directory_name):
+def convertImageToBinaryFile(image_fname,output_fname):
 	header=np.zeros(4).astype(np.int32)
+	image_info=cv2.imread(input_directory_name+file_name)
+        data_file=open(output_directory_name+output_file_name,'w')
+        data_file.write(header.tobytes()+image_info.flatten().tobytes())
+        data_file.close()
+
+def performConversion(input_directory_name,output_directory_name):
 	if not (os.path.isdir(output_directory_name)):
 		os.system("mkdir "+output_directory_name)
 	for file_name in sorted(os.listdir(input_directory_name)):
-		output_file_name=file_name.split('.')[0]+'.bix'
-		print("File name = "+file_name)
-		image_info=misc.imread(input_directory_name+file_name)
-		data_file=open(output_directory_name+output_file_name,'w')
-		data_file.write(header.tobytes()+image_info.flatten().tobytes())
-		data_file.close()
+		convertImageToBinaryFile(file_name,file_name.split('.')[0]+'.bix')
 
-input_dir='../'
-performConversion(input_dir+'images/',input_dir+'/images_raw/')
-		
-print("Finished")
+assert sys.argv == 3, 'Input and output files should be privided as arguments.'
+input_dir= sys.argv[1]
+putput_dir=sys.argv[2]
+performConversion(input_dir,output_dir')
 
