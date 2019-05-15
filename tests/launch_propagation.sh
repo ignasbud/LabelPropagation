5836d9bd46531340bd9beb9ab6c44b423800a542
Ignas Budvytis <ib255@cam.ac.uk>
2019-05-15 11:30:10 +0100

    updated launch propagation script

--- tests/launch_propagation.sh
+++ tests/launch_propagation.sh
@@ -1,2 +1,5 @@
-GPUID=3
-python ../scripts/experiment_launch_propagation.py config_camvid360.json $GPUID ./outputs/project_camvid360/images_binary/R0010094_20170622125256_er_f_00008010.bin
+GPUID=0
+CONFIG='config_camvid360.json'
+SOURCE_IMAGE='../sample_outputs/camvid360/images_binary/R0010094_20170622125256_er_f_00008010.bin'
+
+python ../scripts/experiment_launch_propagation.py $CONFIG $GPUID $SOURCE_IMAGE
