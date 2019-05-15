91974ae5edca635fc02aab2b43ffccd8f7d17594
Ignas Budvytis <ib255@cam.ac.uk>
2019-05-15 10:50:57 +0100

    some updates

--- scripts/experiment_create_binary_images.py
+++ scripts/experiment_create_binary_images.py
@@ -17,7 +17,7 @@ class ExperimentBinaryImages(Experiment):
 		copyfile(self.args.config_file_name, self.config_info["output_directory_info"]["project_dir"]+'config_create_images.json')
 
 	def executeExperiment(self):
-		print('Creating binary images')
+		print('Creating binary images in folder: '+self.config_info['output_directory_info']['binary_images_dir'])
 		[convertImageToBinaryFile(file_name,self.config_info['output_directory_info']['binary_images_dir']+os.path.splitext(os.path.basename(file_name))[0]+'.bin',self.config_info['algorithm_info']['width'],self.config_info['algorithm_info']['height']) for file_name in sorted(glob.glob(self.config_info['input_directory_info']['images_dir']+'*.png'))]
 		#Parallel(n_jobs=multiprocessing.cpu_count())(delayed(saveCombinedLabelFiles)(file_name+'_LAB.png',[file_name+'_O.png',file_name+'_OR.png'],self.config_info['algorithm_info']['width'],self.config_info['algorithm_info']['height'],self.config_info['algorithm_info']['propagation_void_color'])
 
