36f347d8e94a5f0c905010e424589a837a8aefd4
Ignas Budvytis <ib255@cam.ac.uk>
2019-05-15 12:00:08 +0100

    rearranged structure

--- calculate_mappings.sh
+++ calculate_mappings.sh
@@ -1,4 +1,4 @@
 GPUID=0
 CONFIG='./configs/config_camvid360.json'
 
-python ../scripts/experiment_calculate_mappings.py $CONFIG $GPUID
+python ./scripts/experiment_calculate_mappings.py $CONFIG $GPUID
--- create_binary_images.sh
+++ create_binary_images.sh
@@ -1,3 +1,3 @@
 CONFIG='./configs/config_camvid360.json'
 
-python ../scripts/experiment_create_binary_images.py $CONFIG
+python ./scripts/experiment_create_binary_images.py $CONFIG
